fab772ab2718e00bcbacd3a24a5f70fc75f9ce57
Dado85 <debasismallick740@gmail.com>
2026-02-01 20:46:19 +0530

    Update app.js

--- Server/src/app.js
+++ Server/src/app.js
@@ -14,7 +14,7 @@ const app = express();
 const server = http.createServer(app);
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://bytemateui.onrender.com",
     credentials: true,
   }),
 );
